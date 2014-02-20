cf8db38a3cd349fadcb2a323a8402755c0e9def1
David Currie <curriedav@gmail.com>
2014-02-20 15:43:48 -0800

    adds httpCollect

---
+++ httpCollect.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+	bl = require('bl');
+
+var urlToFetch = process.argv[2];
+
+http.get(urlToFetch, function(response) {
+	response.pipe(bl(function (err, data){
+		if (err) {
+			return console.log(err)
+		}
+
+		data = data.toString();
+		console.log(data.length);
+		console.log(data);
+	}))
+});
